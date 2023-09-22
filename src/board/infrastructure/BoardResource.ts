4ff9b61600c05810deac9eb5151fb5233dc8f658
Sanghoon Lee <gcccompil3r@gmail.com>
2023-09-22 11:47:36 +0900

    feat: make basic infra

---
+++ src/board/infrastructure/BoardResource.ts
@@ -0,0 +1,55 @@
+import { RestClient } from '@/utility/RestClient';
+import { BoardRepository } from '@/board/repository/BoardRepository';
+import { BoardId, BoardToSave } from "@/board/types";
+import { Board } from "@/board/Board";
+import { ApiBoard } from "@/board/infrastructure/ApiBoard";
+import { BoardStore } from "@/board/infrastructure/BoardStore";
+
+export class BoardResource implements BoardRepository {
+  constructor(
+    private readonly restClient: RestClient,
+    private readonly store: BoardStore
+  ) {}
+
+  async getBoardList(): Promise<Board[]> {
+    const boardListInStore = this.store.boardList;
+    if (boardListInStore.length !== 0) {
+      return boardListInStore.map(Board.fromProperties);
+    }
+
+    const response = await this.restClient.get<ApiBoard[]>(
+      '/board/list'
+    );
+    const apiBoardList = response.data
+    const boardList = apiBoardList.map((apiBoard: ApiBoard) => apiBoard.toDomain());
+    this.store.saveBoardList(boardList.map((board: Board) => board.properties));
+
+    return boardList;
+  }
+
+  getBoard(boardId: BoardId): Promise<Board> {
+    throw new Error("Method not implemented.");
+  }
+
+  async createBoard(form: BoardToSave): Promise<Board> {
+    const apiBoard = await this.restClient.post<ApiBoard, BoardToSave>(
+      '/board/register',
+      form
+    );
+
+    return apiBoard.toDomain();
+  }
+
+  async updateBoard(boardId: BoardId, form: BoardToSave): Promise<Board> {
+    const apiBoard = await this.restClient.put<ApiBoard, BoardToSave>(
+        `/board/${boardId}`,
+        form
+      );
+  
+      return apiBoard.toDomain();
+    }
+
+    async deleteBoard(boardId: BoardId): Promise<void> {
+        await this.restClient.delete(`/board/${boardId}`);
+    }
+}
--- src/board/infrastructure/BoardStore.ts
+++ src/board/infrastructure/BoardStore.ts
@@ -1,12 +1,12 @@
 import create from 'zustand';
-import {BoardProperties} from "@/board/types";
+import { BoardProperties } from "@/board/types";
 
-type BoardStore = {
-    boardList: BoardProperties[];
-    saveBoardList: (boardList: BoardProperties[]) => void;
+export type BoardStore = {
+  boardList: BoardProperties[];
+  saveBoardList: (boardList: BoardProperties[]) => void;
 };
 
-export const useRecipeStore = create<BoardStore>((set) => ({
-    boardList: [],
-    saveBoardList: (boardList) => set({ boardList }),
+export const useBoardStore = create<BoardStore>((set) => ({
+  boardList: [],
+  saveBoardList: (boardList) => set({ boardList }),
 }));
